b308f46e602221de7523d15ee12531266ba0bcac
JihoKim0 <kjhjulie2000@gmail.com>
2022-06-17 14:01:22 +0900

    TCP daytime service

---
+++ NetworkProgramming/daytime_tcp_client.c
@@ -0,0 +1,53 @@
+//20220413
+//receive current time from server and print
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <time.h>
+
+#define BUFF 1024
+
+int main(int argc, char** argv) {
+    int sock;
+    char msg[BUFF];
+    int str_len;
+    struct sockaddr_in serv_addr;
+    time_t tim;
+    tim = time(NULL);
+    if (argc != 3) {
+        printf("Usage: %s <IP> <port> \n", argv[0]);
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket creation error");
+        exit(1);
+
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect error");
+        exit(1);
+    }
+
+    strcpy(msg, "현재 시각은 ");
+    strcat(msg, ctime(&tim));
+    write(sock, msg, strlen(msg));
+    str_len = read(sock, msg, BUFF - 1);
+    msg[str_len] = 0;
+    printf("Message from a server: %s \n", msg);
+
+    close(sock);
+
+    return 0;
+}
